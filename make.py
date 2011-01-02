03cac561107d78518982b1111a0668345f8af9ee
Bruno Calado Ribeiro <plus.ultra.physis@gmail.com>
2011-01-02 15:15:24 -0200

    There is a python3 script which compiles and generate the executable for the binary gene sample.

---
+++ make.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+#----------------------------------------------------------
+# Compile and generate the executable for the binary gene
+# sample
+#----------------------------------------------------------
+
+
+
+import os, sys
+
+#----------------------------------------------------------
+# Variables and Constants
+#----------------------------------------------------------
+#dir receives the actual path 
+dir = os.getcwd()
+
+makepyDir = ''
+
+compilerOptionsFileName = 'compilerOptions.cfg'
+sourceFilesFileName = 'sourceFiles.cfg'
+
+compiler = 'ifort'
+extension = 'f90'
+
+compilerOptions = []
+sourceDirs = [ 'app/', 'genes/', 'src/' ]
+sourceFiles = []
+selectedSourceFiles = []
+exeFilename = 'FORGATRAN'
+
+
+out = ''
+outSrc= ''
+debug = 0
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2: 
+        if '-' in sys.argv[1]: 
+            if sys.argv[1] == '-config': 
+ 
+                fileHandle = open( sourceFilesFileName, 'w')
+                for i in sourceDirs:
+                    for j in os.listdir(i):
+                        if extension in j:
+                            fileHandle.writelines( '1 ' + i+j  + '\n')
+                fileHandle.close()
+                sys.exit(None)
+            elif 'debug' in sys.argv[1]: 
+                debug = 1
+            else:
+                exeFilename = sys.argv[1]  
+
+
+        else:
+            compilerOptionsFileName = sys.argv[1]
+ 
+
+
+    fileHandle = open( sourceFilesFileName, 'r')
+    sourceFiles = fileHandle.read().splitlines()
+    fileHandle.close()
+
+    if debug:
+        print( '\n' + 50*'-' )
+        print( 'Files to be compiled:' )  
+        
+    for i in range(0, len(sourceFiles)):
+        if '1' in sourceFiles[i].split()[0]:
+            if debug: print( '1 -> ' + sourceFiles[i].split()[1] )
+            selectedSourceFiles.append(sourceFiles[i].split()[1])
+        else:
+            if debug: print( '0 -> ' + sourceFiles[i].split()[1] )
+
+    if debug: print( 50*'-' + '\n' )
+
+
+
+#----------------------------------------------------------
+# Compiler Options
+#----------------------------------------------------------
+#Open files with the compiler flags
+fileHandle = open( compilerOptionsFileName, 'r')
+compilerOptions = (fileHandle.read()).splitlines()
+fileHandle.close()
+
+#Temporary variable
+out = compiler
+
+
+if debug:
+    print( '\n' + 50*'-' )
+    print( 'Selected options:' )  
+for i in compilerOptions:
+    if debug: print( i )
+    out += ' ' + i
+out += ' ' + exeFilename
+if debug: print( 50*'-' + '\n' )
+
+
+
+
+
+#----------------------------------------------------------
+# Main
+#----------------------------------------------------------
+outtmp = out
+for j in range(0,3):
+    out = outtmp
+    for i in selectedSourceFiles:
+        out += ' ' + i
+
+    if debug: os.system('echo ' + out)
+
+    os.system(out)
+
+
+if debug: print( 'Success' )
+
+os.system('rm -f *.mod ')
+
+
+
+
